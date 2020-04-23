ab0eab4cf1017db0a716654449816dbb38638040
Rjay012 <rjhaybaterina12@gmail.com>
2020-04-23 15:13:59 +0800

    ready for design

---
+++ OECS/Scripts/Custom_Scripts/Dashboard.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    FetchData("/Dashboard/SideMenu", null).done(function (header) {
+        //$("header").html(header);
+        // SideNav Initialization
+        $(".button-collapse").sideNav();
+
+        var container = document.querySelector('.custom-scrollbar');
+        Ps.initialize(container, {
+            wheelSpeed: 2,
+            wheelPropagation: true,
+            minScrollbarLength: 20
+        });
+
+        // Tooltips Initialization
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip();
+        });
+    });
+});
