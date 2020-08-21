110ea62d0ae9a91759ef0d7c78576cfc56d9e69a
RJay Baterina <40509406+Rjay012@users.noreply.github.com>
2020-08-21 14:53:24 +0800

    Finalized Checkout Prototype

---
+++ OECS/Controllers/CheckOutController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OECS.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        // GET: CheckOut
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult LoadPackage()
+        {
+            return PartialView("Partials/Tables/_Package");
+        }
+
+        public ActionResult ShippingAndOrderSummary()
+        {
+            return PartialView("Partials/Cards/_ShippingAndOrderSummary");
+        }
+    }
+}
---
+++ OECS/Scripts/Custom_Scripts/Main_Scripts/CheckOut.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    LoadPackage();
+    LoadShippingAndOrderSummary();
+});
+
+function LoadPackage() {
+    FetchData("/CheckOut/LoadPackage", null).done(function (content) {
+        $(".checkout-package").html(content);
+    });
+}
+
+function LoadShippingAndOrderSummary() {
+    FetchData("/CheckOut/ShippingAndOrderSummary", null).done(function (content) {
+        $(".checkout-shipping-order-summary").html(content);
+    });
+}
