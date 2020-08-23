84417001b0cc41b3def81dc4306e11e97c7bcb8f
RJay Baterina <40509406+Rjay012@users.noreply.github.com>
2020-08-23 09:32:12 +0800

    Finish Final Order Process Design and Finishing customer ordering prototype

---
+++ OECS/Controllers/OrderPurchasedController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OECS.Controllers
+{
+    public class OrderPurchasedController : Controller
+    {
+        // GET: OrderPurchased
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PurchasedGreetingCard()
+        {
+            return PartialView("Partials/Cards/_PurchasedGreetings");
+        }
+
+        public ActionResult OrderDetailSummaryCard()
+        {
+            return PartialView("Partials/Cards/_OrderDetails");
+        }
+
+        public ActionResult PurchasedItemTable()
+        {
+            return PartialView("Partials/Tables/_PurchasedItem");
+        }
+
+        public ActionResult OrderSummaryAccordion()
+        {
+            return PartialView("Partials/Accordions/_OrderSummary");
+        }
+    }
+}
---
+++ OECS/Scripts/Custom_Scripts/Main_Scripts/OrderPurchased.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    ShowPurchasedGreetingCard();
+    ShowOrderDetailsSummaryCard();
+});
+
+function ShowPurchasedGreetingCard() {
+    LoadPartials("PurchasedGreetingCard", ".purchased-greeting-card");
+}
+
+function ShowOrderDetailsSummaryCard() {
+    LoadPartials("OrderDetailSummaryCard", ".order-details");
+    LoadPartials("PurchasedItemTable", "#PurchasedItemTable");
+    LoadPartials("OrderSummaryAccordion", "#OrderSummaryAccordion");
+}
+
+function LoadPartials(actionMethod, container) {
+    FetchData("/OrderPurchased/" + actionMethod, null).done(function (content) {
+        $(container).html(content);
+    });
+}
